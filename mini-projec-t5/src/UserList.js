6488ae30d08f3f52c72e4a595e35efccea3c1d9c
AhmedMahamoud244 <120144072+AhmedMahamoud244@users.noreply.github.com>
2023-11-03 16:14:20 -0400

    Miniproject7

---
+++ mini-projec-t5/src/UserList.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
+function UsersList() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/users')
+            .then(response => response.json())
+            .then(data => {
+                setUsers(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error("Error fetching users:", error);
+                setLoading(false);
+            });
+    }, []);  // The empty dependency array ensures this effect runs once when the component mounts.
+
+    if (loading) return <div>Loading...</div>;
+
+    return (
+        <div>
+            <h1>Users List</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user._id}>
+                        {user.name} ({user.age} years old) - {user.email}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default UsersList;
--- server.js
+++ server.js
@@ -1,27 +1,75 @@
-const http = require('http');
+const express = require('express');
+const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
- 
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/api/users') {
-    // Read user data from a file using fs and path modules
-    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-        return;
-      }
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(data);
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
+const cors = require('cors');
+
+const app = express();
+
+// Configuration
+const PORT = 3000;
+const MONGODB_URI = 'mongodb://localhost:27017/Mini-project7';
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB', err);
 });
- 
-// Start the server on port 3000
-server.listen(3000, () => {
-  console.log('Server running on <http://localhost:3000/>');
+
+const db = mongoose.connection;
+
+// Define MongoDB Schema and Model
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Define API Endpoints
+
+// Get all users
+app.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).send('Error fetching users');
+        console.error('Error fetching users:', err);
+    }
+});
+
+// Add a new user
+app.post('/api/users', async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.json(newUser);
+    } catch (err) {
+        res.status(500).send('Error adding user');
+        console.error('Error adding user:', err);
+    }
+});
+
+// Fallback for serving 'users.json'
+app.get('/users.json', (req, res) => {
+    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}/`);
 });
